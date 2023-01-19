2c5950121af43c36c15650579f6f768fc9f1db01
Ar-Dante <atlas.monker@gmail.com>
2023-01-19 11:51:11 +0200

    update to HW12

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from collections import UserDict
 from datetime import datetime
 import re
+import pickle
 
 
 class Field:
     def __init__(self, value):
         self.value = value
 
-
 class Name(Field):
     pass
 
 
 class Phone(Field):
+
     @property
     def value(self):
         return self.__value
@@ -39,6 +40,7 @@ class Birthday(Field):
         else:
             print("Неправильний формат даних, має бути ДД-ММ-РРРР")
 
+
 class Record(Field):
     def __init__(self, name, phone=None, birthday=None):
         self.name = Name(name)
@@ -47,6 +49,7 @@ class Record(Field):
             self.phone = [Phone(phone)]
         else:
             self.phone = []
+
     def days_to_birthday(self):
         d1 = datetime.today()
         d2 = datetime.strptime(self.bd.value, "%d-%m-%Y")
@@ -68,7 +71,7 @@ class Record(Field):
 
 class AddressBook(UserDict):
     def add_record(self, rec):
-        self.data[rec.name.value] = rec
+        self.data[rec.name.value] = rec.phone[0].value
 
     def print_n_item(self, N=2):
         page = 1
@@ -96,6 +99,22 @@ class AddressBook(UserDict):
             if next:
                 continue
 
+    def find(self, item):
+        for name, phone in self.data.items():
+            if name == item or phone == item:
+                print(f'Name: {name}; Phone: {phone} ')
+
+def to_pickle(item):
+    file_name = 'data.bin'
+    with open(file_name, "wb") as fh:
+        pickle.dump(item, fh)
+        print("Сереалізація пройшла успішно")
+
+def from_pickle(file_name):
+    with open(file_name, "rb") as fh:
+        print(pickle.load(fh))
+
+
 
 if __name__ == '__main__':
     name = 'Bill'
@@ -104,12 +123,12 @@ if __name__ == '__main__':
     rec = Record(name, phone, birthday)
     print(rec.days_to_birthday())
     ab = AddressBook()
-    rec1 = Record("A", '1312312', '01-02-3423')
+    rec1 = Record("Bill", '1312312', '01-02-3423')
     rec2 = Record("B", '1213312312', '11-02-3423')
     rec3 = Record("C", '131231312', '12-02-3423')
-    rec4 = Record("D", '131232312', '13-02-1423')
-    rec5 = Record("E", '1312312312', '14-02-3423')
-    rec6 = Record("F", '1312311232', '15-02-3423')
+    rec4 = Record("D", '131231312', '13-02-1423')
+    rec5 = Record("E", '131231312', '14-02-3423')
+    rec6 = Record("F", '131231312', '15-02-3423')
     ab.add_record(rec)
     ab.add_record(rec1)
     ab.add_record(rec2)
@@ -117,11 +136,9 @@ if __name__ == '__main__':
     ab.add_record(rec4)
     ab.add_record(rec5)
     ab.add_record(rec6)
-    assert isinstance(ab['Bill'], Record)
-    assert isinstance(ab['Bill'].name, Name)
-    assert isinstance(ab['Bill'].phone, list)
-    assert isinstance(ab['Bill'].phone[0], Phone)
-    assert ab['Bill'].phone[0].value == '1234567890'
-    print(ab.data)
 
+    print(ab.data)
+    ab.find('131231312')
+    to_pickle(ab.data)
+    from_pickle('data.bin')
     print('Test DONE!')
